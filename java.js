b61bab6a8d5c1e7121659f5d0dbc21dd094c8f23
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-02-18 09:18:54 +0100

    begynn

---
+++ java.js
@@ -0,0 +1,9 @@
+function darkTheme() {
+    var theme = document.getElementById('darkTheme');
+    theme.href = "dark-stilark.css";
+  }
+  
+  function lightTheme() {
+    var theme = document.getElementById('lightTheme');
+    theme.href = "stilark.css";
+  }
